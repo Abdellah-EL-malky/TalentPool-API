32eed32e3e1b25892b1f69743d1df3babaaebfe7
Abdellah <abdel.elmalky@gmail.com>
2025-03-28 12:34:20 +0000

    ajout de la classe announcement

--- app/Models/Announcement.php
+++ app/Models/Announcement.php
@@ -2,7 +2,21 @@
 
 namespace App\Models;
 
-class Announcement
-{
+use Illuminate\Database\Eloquent\Model;
 
+class Announcement extends Model
+{
+    protected $fillable = [
+        'user_id', 'title', 'description', 'company', 'location', 'status'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function applications()
+    {
+        return $this->hasMany(Application::class);
+    }
 }
